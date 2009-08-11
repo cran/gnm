f3c2debc71be70a8a5b51c6015a7ca1248f8f286
Heather Turner <Heather.Turner@warwick.ac.uk>
2009-08-11 00:00:00 +0000

    version 0.10-0

--- R/expandCategorical.R
+++ R/expandCategorical.R
@@ -1,13 +1,36 @@
 expandCategorical <- function(data, catvar, sep = ".", countvar = "count",
-                              idvar = "id", as.ordered = FALSE) {
-    n <- nrow(data)
-    inter <- interaction(data[[catvar]])
-    ncat <- nlevels(inter)
-    caseID <- gl(n, ncat)
-    newData <- data[caseID, -match(catvar, names(data))]
-    newData[[levels(interaction(catvar, sep = sep))]] <-
-        gl(ncat, 1, n * ncat, labels = levels(inter), ordered = as.ordered)
-    newData[[countvar]] <- as.vector(t(class.ind(inter)))
-    newData[[idvar]] <- caseID
+                              idvar = "id", as.ordered = FALSE, group = TRUE) {
+
+    cat <- interaction(data[catvar], sep = sep)
+    ncat <- nlevels(cat)
+
+    if (group == TRUE) {
+        data <- data[, -match(catvar, names(data))]
+        ord <- do.call("order", data)
+        vars <- data[ord,]
+        dupvars <- duplicated(vars)
+        reps <- rle(dupvars)
+        n <- sum(!dupvars)
+        ni <- rep(1, n)
+        grp.ni <- reps$lengths[reps$values] + 1
+        grps <- cumsum(reps$lengths[!reps$values])[seq(grp.ni)]
+        ni[grps] <- grp.ni
+        id <- factor(rep(seq(n), ni))
+
+        counts <- as.data.frame(table(list(cat = cat[ord], id = id)))
+
+        newData <- vars[which(!dupvars)[counts$id],]
+        rownames(newData) <- NULL
+        newData[c(catvar, idvar, countvar)] <- counts
+    }
+    else {
+        n <- nrow(data)
+        id <- gl(n, ncat)
+        newData <- data[id, -match(catvar, names(data))]
+        newData[[levels(interaction(catvar, sep = sep))]] <-
+            gl(ncat, 1, n * ncat, labels = levels(cat), ordered = as.ordered)
+        newData[[countvar]] <- as.vector(t(class.ind(cat)))
+        newData[[idvar]] <- id
+    }
     newData
 }
